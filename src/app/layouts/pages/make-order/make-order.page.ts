148e78e26df93e1653b035c15d2504e0f4f65d3a
denmose1 <dennombongi6@gmail.com>
2019-06-18 10:54:26 +0300

    first commit

---
+++ src/app/layouts/pages/make-order/make-order.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-make-order',
+  templateUrl: './make-order.page.html',
+  styleUrls: ['./make-order.page.scss'],
+})
+export class MakeOrderPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
